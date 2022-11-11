7f98da9167b4494fc61a6708a4d4404605bc5223
Thorsten Blass <Thorsten.Blass@amd.com>
2022-11-11 12:48:06 +0000

    - formatted vectoradd.c using clang-format

--- examples/c/vectoradd/vectoradd.c
+++ examples/c/vectoradd/vectoradd.c
@@ -1,43 +1,43 @@
 
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
 
 // simple example of vector addition combined with AMD GPU offloading
 // to keep it simple there is no error handling (e.g. malloc fails)
-int main(int argc, char** argv){
+int main(int argc, char **argv) {
 
-    int nsize = 1000000;
-    int *a, *b, *c;
+  int nsize = 1000000;
+  int *a, *b, *c;
 
-    a = (int*) malloc(nsize * sizeof(int));
-    b = (int*) malloc(nsize * sizeof(int));
-    c = (int*) malloc(nsize * sizeof(int));
+  a = (int *)malloc(nsize * sizeof(int));
+  b = (int *)malloc(nsize * sizeof(int));
+  c = (int *)malloc(nsize * sizeof(int));
 
-    //memset((void*) a, 0, nsize);
+  // memset((void*) a, 0, nsize);
 
-    for(int i = 0; i < nsize; i++){
-      b[i] = 1;
-      c[i] = 2;   
+  for (int i = 0; i < nsize; i++) {
+    b[i] = 1;
+    c[i] = 2;
+  }
+
+#pragma omp target teams distribute parallel for map(from : a) map(to : b, c)
+  for (int i = 0; i < nsize; i++) {
+    a[i] = b[i] + c[i];
+  }
+
+  for (int i = 0; i < nsize; i++) {
+    if (a[i] != 3) {
+      printf("Found wrong value! Found %u expected 3\n", a[i]);
+      return EXIT_FAILURE;
     }
+  }
 
-    #pragma omp target teams distribute parallel for map(from:a) map(to:b,c)
-    for(int i = 0; i < nsize; i++){
-        a[i] = b[i] + c[i];
-    }
+  printf("All values are correct!\n");
 
-    for(int i = 0; i < nsize; i++){
-        if(a[i] != 3){
-            printf("Found wrong value! Found %u expected 3\n", a[i]);
-            return 1;
-        }
-    }
+  free(a);
+  free(b);
+  free(c);
 
-    printf("All values are correct!\n");
-
-    free(a);
-    free(b);
-    free(c);
-
-    return 0;
+  return EXIT_SUCCESS;
 }
